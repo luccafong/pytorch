53401fa6042fb68456358bc31f48ed7a6ea1e2b2
Brian Hirsh <hirsheybar@fb.com>
2025-10-14 14:58:00 -0700

    add ability to disable functionalization in AOTDispatcher
    ghstack-source-id: c9860dc44f513f46d2991ce07eb5dee269b82d01
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/164577

--- test/functorch/test_aotdispatch.py
+++ test/functorch/test_aotdispatch.py
@@ -2607,6 +2607,105 @@ def forward(self, primals_1, primals_2):
         ]
         self.verify_aot_autograd(f, inp_grad, test_mutation=True)
 
+    def test_fw_bw_mutation_no_functionalization1(self):
+        class FwBwMutation(torch.autograd.Function):
+            @staticmethod
+            def forward(ctx, a, b):
+                # input mutation
+                torch._foreach_mul_([b], [2])
+                x = b + 1
+                # intermediate mutation
+                torch._foreach_mul_([x], [3])
+                ctx.save_for_backward(x)
+                return x * a
+
+            @staticmethod
+            def backward(ctx, grad_output):
+                (x,) = ctx.saved_tensors
+                # bw mutation
+                torch._foreach_mul_([x], [4])
+                return grad_output * x, grad_output * x
+
+        def f(a, b):
+            return FwBwMutation.apply(a, b)
+
+        inps = [
+            torch.ones(3, 3, requires_grad=True),
+            torch.ones(3, 3, requires_grad=False),
+        ]
+        inps_ref = [
+            torch.ones(3, 3, requires_grad=True),
+            torch.ones(3, 3, requires_grad=False),
+        ]
+
+        compiled_f = compiled_function(
+            f,
+            nop,
+            nop,
+            dynamic=False,
+            partition_fn=default_partition,
+            keep_inference_input_mutations=True,
+            disable_functionalization=True,
+        )
+
+        out_ref = f(*inps_ref)
+        out = compiled_f(*inps)
+        self.assertEqual(out, out_ref)
+
+        out_ref.sum().backward()
+        out.sum().backward()
+        self.assertEqual(inps_ref[0].grad, inps[0].grad)
+
+    def test_fw_bw_mutation_no_functionalization2(self):
+        class FwBwMutation(torch.autograd.Function):
+            @staticmethod
+            def forward(ctx, x):
+                # input mutation
+                torch._foreach_mul_([x], [2])
+                x = x + 1
+                # intermediate mutation
+                torch._foreach_mul_([x], [3])
+                ctx.save_for_backward(x)
+                return x
+
+            @staticmethod
+            def backward(ctx, grad_output):
+                (x,) = ctx.saved_tensors
+                # bw mutation
+                torch._foreach_mul_([x], [4])
+                return grad_output * x
+
+        def f(a, b):
+            out = FwBwMutation.apply(b)
+            return out * a
+
+        inps = [
+            torch.ones(3, 3, requires_grad=True),
+            torch.ones(3, 3, requires_grad=False),
+        ]
+        inps_ref = [
+            torch.ones(3, 3, requires_grad=True),
+            torch.ones(3, 3, requires_grad=False),
+        ]
+
+        compiled_f = compiled_function(
+            f,
+            nop,
+            nop,
+            dynamic=False,
+            partition_fn=default_partition,
+            keep_inference_input_mutations=True,
+            disable_functionalization=True,
+        )
+
+        out_ref = f(*inps_ref)
+        out = compiled_f(*inps)
+        self.assertEqual(out, out_ref)
+
+        out_ref.sum().backward()
+        out.sum().backward()
+        self.assertEqual(inps_ref[0].grad, inps[0].grad)
+
     def test_backward_mutation_metadata(self):
         class BwMutation(torch.autograd.Function):
             @staticmethod
@@ -8059,7 +8158,15 @@ symbolic_aot_autograd_failures = {
 }
 
 
-def _test_aot_autograd_helper(self, device, dtype, op, dynamic=False, use_min_cut=True):
+def _test_aot_autograd_helper(
+    self,
+    device,
+    dtype,
+    op,
+    dynamic=False,
+    use_min_cut=False,
+    disable_functionalization=False,
+):
     if not op.supports_autograd:
         self.skipTest("Op does not support autograd")
 
@@ -8091,6 +8198,7 @@ def _test_aot_autograd_helper(self, device, dtype, op, dynamic=False, use_min_cu
                 try_check_data_specialization=try_check_data_specialization,
                 skip_correctness_check=op.skip_correctness_check_compile_vs_eager,
                 use_min_cut=use_min_cut,
+                disable_functionalization=disable_functionalization,
             )
         except DynamicOutputShapeException:
             self.skipTest("Dynamic output shape operation in trace")
@@ -8194,24 +8302,32 @@ class TestEagerFusionOpInfo(AOTTestCase):
     @ops(op_db + hop_db, allowed_dtypes=(torch.float,))
     @skipOps(
         "TestEagerFusionOpInfo",
-        "test_aot_autograd_default_partition_exhaustive",
+        "test_aot_autograd_disable_functionalization_exhaustive",
         aot_autograd_failures,
     )
-    def test_aot_autograd_default_partition_exhaustive(self, device, dtype, op):
-        _test_aot_autograd_helper(self, device, dtype, op, use_min_cut=False)
+    def test_aot_autograd_disable_functionalization_exhaustive(self, device, dtype, op):
+        _test_aot_autograd_helper(
+            self, device, dtype, op, use_min_cut=False, disable_functionalization=True
+        )
 
     @ops(op_db + hop_db, allowed_dtypes=(torch.float,))
     @patch("functorch.compile.config.debug_assert", True)
     @skipOps(
         "TestEagerFusionOpInfo",
-        "test_aot_autograd_symbolic_default_partition_exhaustive",
+        "test_aot_autograd_disable_functionalization_symbolic_exhaustive",
         aot_autograd_failures | symbolic_aot_autograd_failures,
     )
-    def test_aot_autograd_symbolic_default_partition_exhaustive(
+    def test_aot_autograd_disable_functionalization_symbolic_exhaustive(
         self, device, dtype, op
     ):
         _test_aot_autograd_helper(
-            self, device, dtype, op, dynamic=True, use_min_cut=False
+            self,
+            device,
+            dtype,
+            op,
+            dynamic=True,
+            use_min_cut=False,
+            disable_functionalization=True,
         )
 
 
